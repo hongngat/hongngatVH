dc2d8934b721734bc8def51abe66e8b1d6b33ab4
hongngat <48482193+hongngat@users.noreply.github.com>
2020-04-08 16:26:09 +0700

    lại

--- img/55_files/ads_code_1.ads
+++
@@ -1,1 +0,0 @@
-var admdetectadb=false;
--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
-$(function() {
-    
-    $("h2")
-        .wrapInner("<span>")
 
-    $("h2 br")
-        .before("<span class='spacer'>")
-        .after("<span class='spacer'>");
+function openNav() {
+    document.getElementById("nav").style.width = "100%";
+  }
+  function closeNav() {
+    document.getElementById("nav").style.width = "0%";
+  }
 
+  $(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+/*sectio-5 */
+  $(document).ready(function() {
+    $('#Carousel').carousel({
+        interval: 5000
+    })
 });
